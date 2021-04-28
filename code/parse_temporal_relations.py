9431d3ace06f589ec6b9d7d8c8b5fdd2d0527436
Alexandra DeLucia <aadelucia@jhu.edu>
2021-04-28 13:52:45 -0400

    repo cleanup

--- code/parse_temporal_relations.py
+++ code/parse_temporal_relations.py
@@ -1,40 +1,58 @@
 """
-Parse raw timeline/predictions from Fine-Grained Temporal
-Extraction output
+Parse raw timeline/predictions from LOME temporal output (Concrete .tar.gz)
 
-Output is single JSONL with a single entry for each Gigaword document
+Output is single JSONL with a single entry for each Gigaword document of form
+
+{"filename": filename, "relations": [(kind, arg0, arg1), (kind, arg0, arg1)]}
+
+where "kind" is one of before/after/contained/container and arg0/arg1 are verbs.
 
 Author: Alexandra DeLucia
 """
 import pandas as pd
-import csv
-import json
 import jsonlines
 from tqdm import tqdm
 from argparse import ArgumentParser
+import pickle
 import logging
+import tarfile
+
+from concrete.communication.ttypes import Communication
+from typing import Dict
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("--input-files", nargs="+", help="List of JSONL files")
-    parser.add_argument("--output-dir", type=str, help="Output folder to save .json")
+    parser.add_argument("--input-file", nargs="+", help="Tarball of Concrete Gigaword files processed through LOME.")
+    parser.add_argument("--output-file", type=str, help="Path to save .jsonl")
     parser.add_argument('--debug', action='store_true')
     parser.add_argument("--log-file", help="Path to log")
     return parser.parse_args()
 
 
-def determine_temporal_relation(event1, event2):
-    # 1. Event1 completed before Event2 started
-    if event1[1] < event2[0]:
-        return "before"
-    # 2. Event2 completed before Event1 started
-    if event1[0] > event2[1]:
-        return "after"
-    # 3. There is overlap
-    return "overlap"
+def get_temporal_relations(comm: Communication):
+    """
+    Get before/after/contained/container temporal relations from text
+    """
+    # 1. Store spans by ID for easy lookup
+    spans_by_id = dict(map(lambda x: (x.uuid.uuidString, x.text), comm.situationMentionSetList[0].mentionList))
+    # logger.debug(f"{span.situationType=} {span.text=} {span.situationKind=} {span.argumentList}")
+
+    # 2. Get temporal relations
+    # sm.confidence was always None so I took it out
+    # situationKind is one of "after", "before", "contained", "container"
+    def extract_relation(sm):
+        return (sm.situationKind,
+                # sm.argumentList[0].role,  # always ARG0
+                # sm.argumentList[1].role,
+                spans_by_id.get(sm.argumentList[0].situationMentionId.uuidString),
+                spans_by_id.get(sm.argumentList[1].situationMentionId.uuidString)
+                )
+    relations = map(extract_relation, comm.situationMentionSetList[1].mentionList)
+    return relations
 
 
 if __name__ == "__main__":
@@ -45,28 +63,16 @@ if __name__ == "__main__":
     if args.log_file:
         logger.addHandler(logging.FileHandler(args.log_file))
 
-    counts = {}
-    all_relations = {}
-    for input_file in tqdm(args.input_files):
-        # Save ID
-        document_id = input_file.split("/")[-1].split(".")[0]
-        all_relations[document_id] = []
+    with tarfile.open(args.input_file) as archive:
+        num_comms = sum(1 for comm in archive if comm.isfile())
 
-        # Columns
-        # 'sent_pred_id1', 'sent_pred_id2', 'b1', 'e1', 'b2', 'e2',
-        # 'pred1_duration', 'pred2_duration', 'pred1_text', 'pred2_text',
-        # 'pred1_dict_idx', 'pred2_dict_idx'
-        with open(input_file) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                event1 = (row['b1'], row['e1'])
-                event2 = (row['b2'], row['e2'])
-                relationship = determine_temporal_relation(event1, event2)
-                all_relations[document_id].append((row['pred1_text'], row['pred2_text'], relationship))
-                counts[relationship] = counts.get(relationship, 0) + 1
-                logger.debug(f"{event1}\t{event2}\t{relationship}")
+    writer = jsonlines.open(args.output_file, "w")
+    for comm, filename in tqdm(CommunicationReader(input_file, total=num_comms)):
+        rel = {
+            "filename": filename,
+            "relations": list(get_temporal_relations(comm))
+        }
+        writer.write(rel)
 
-    with open(f"{args.output_dir}/temporal_relations.json", "w") as f:
-        json.dump(all_relations, f)
-
-    logger.info(counts)
+    writer.close()
+    logger.info("Done")
--- scripts/temporal_relation.sh
+++ scripts/temporal_relation.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-INPUT=(${EVENT_HOME}/test/*.output_predictions.csv)
-OUTPUT="${EVENT_HOME}/test"
-mkdir -p "${OUTPUT}"
+INPUT="${EVENT_HOME}/test/NYT_comms.tar.gz"
+OUTPUT="${EVENT_HOME}/test/temporal_relations.jsonl"
 
 python "${EVENT_HOME}/code/parse_temporal_relations.py" \
-  --input-files "${INPUT[@]}" \
+  --input-files "${INPUT}" \
   --output-dir "${OUTPUT}" \
-  --debug
